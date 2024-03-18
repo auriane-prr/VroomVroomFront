6ff1f90bef5d97d506d3e1d002ecf0adcabdac04
auriane-prr <auriane.poirier@etu.umontpellier.fr>
2024-03-18 11:04:54 +0100

    ajout App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './App.css'; 
+import voiture from './voiture.png';
 
 function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [isConnected, setIsConnected] = useState(false);
   const gridSize = 10;
-  const caseSize = 50;
   const socketRef = useRef(null); 
   const [validPositions, setValidPositions] = useState([]);
   const [elapsedTime, setElapsedTime] = useState(0);
+  const [carDirection, setCarDirection] = useState(0);
+  const [gameStatus, setGameStatus] = useState("");
+  const [showPenalty, setShowPenalty] = useState(false);
+
 
 
   const connectWebSocket = () => {
@@ -15,11 +20,12 @@ function App() {
   
     socketRef.current.onopen = () => {
       console.log("WebSocket connection established");
-      socketRef.current.send("START"); // Demande à la fois la position initiale et le chemin
+      socketRef.current.send("START"); 
     };
   
     socketRef.current.onmessage = (event) => {
       console.log("Message from server", event.data);
+
       if (event.data.startsWith("Nouvelle position:") || event.data.startsWith("Initial position:")) {
         const newPosition = parsePositionFromResponse(event.data.substring("Initial position:".length));
         setPosition(newPosition);
@@ -27,17 +33,21 @@ function App() {
         const pathData = event.data.substring("Path:".length);
         const positions = JSON.parse(pathData).map(pos => ({
           x: pos.x,
-          y: gridSize - 1 - pos.y // Ajuster chaque y pour l'inversion
+          y: gridSize - 1 - pos.y 
       }));
-    setValidPositions(positions);
-    } else if (event.data.startsWith("Victory:")) {
-      alert("Victoire !");
-    } else if (event.data.startsWith("Defeat:")) {
-      alert("Défaite !");
-    } else if (event.data.startsWith("ElapsedTime:")) {
-      const time = parseInt(event.data.split(":")[1].trim(), 10);
-      setElapsedTime(time);
-    }
+      setValidPositions(positions);
+      } else if (event.data.startsWith("Victory:")) {
+        setGameStatus("victory");
+      } else if (event.data.startsWith("Defeat:")) {
+        setGameStatus("defeat");
+      } else if (event.data.startsWith("ElapsedTime:")) {
+        const time = parseInt(event.data.split(":")[1].trim(), 10);
+        setElapsedTime(time);
+      } else if (event.data.startsWith("Penalty:")) {
+        console.log("Pénalité de 1 secondes");
+        setShowPenalty(true);
+        setTimeout(() => setShowPenalty(false), 2000);
+      }
     };
   
     socketRef.current.onclose = () => {
@@ -59,25 +69,38 @@ function App() {
     const handleKeyDown = (event) => {
       event.preventDefault();
       let direction;
+      let rotation = carDirection;
 
       switch (event.key) {
-        case 'ArrowUp': direction = "UP"; break;
-        case 'ArrowDown': direction = "DOWN"; break;
-        case 'ArrowLeft': direction = "LEFT"; break;
-        case 'ArrowRight': direction = "RIGHT"; break;
-        default: return; // Quitter si une touche non gérée est pressée
+        case 'ArrowUp': 
+        direction = "UP"; 
+        rotation = 0; 
+        break;
+        case 'ArrowDown': 
+        direction = "DOWN";
+        rotation = 180; 
+        break;
+        case 'ArrowLeft': 
+        direction = "LEFT"; 
+        rotation = 270;
+        break;
+        case 'ArrowRight': 
+        direction = "RIGHT"; 
+        rotation = 90;
+        break;
+        default: return; 
+        
       }
+      setCarDirection(rotation);
 
-      // Envoyer la direction via WebSocket
       if (socketRef.current) {
         socketRef.current.send(direction);
       }
     };
 
-    // Ajouter l'écouteur d'événement pour les touches
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
-  }, []); 
+  }, [isConnected]); 
 
   const parsePositionFromResponse = (responseBody) => {
     const match = responseBody.match(/X=(\d+), Y=(\d+)/);
@@ -90,48 +113,72 @@ function App() {
     return { x: 0, y: 0 };
   };
   
+  const Modal = ({ onStartClick }) => (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Prêt?</h2>
+        <p>Il faut faire le parcours en moins de 10 seconde,</p>
+        <p>Prêt, feu, partez !</p>
+        <button onClick={onStartClick}>GO</button>
+      </div>
+    </div>
+  );
 
+  const VictoryModal = ({ onRestart }) => (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Victoire !</h2>
+        <button onClick={onRestart}>Rejouer</button>
+      </div>
+    </div>
+  );
+  
+  const DefeatModal = ({ onRestart }) => (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Défaite !</h2>
+        <button onClick={onRestart}>Rejouer</button>
+      </div>
+    </div>
+  );
+
+  const handleRestart = () => {
+    setGameStatus("");
+    setIsConnected(false);
+  };
 
   const renderGrid = () => {
     let grid = [];
-    // Inverser l'ordre des lignes pour que y=0 soit en bas
+    const imagePath = voiture;
     for (let y = gridSize - 1; y >= 0; y--) {
       let row = [];
-      // Générer les colonnes normalement
       for (let x = 0; x < gridSize; x++) {
         let isLetterPosition = x === position.x && y === position.y;
-        let isValidPosition = validPositions.some(pos => pos.x === x && pos.y === (gridSize - 1 - y)); // Vérifiez si la position actuelle est une position valide
+        let isValidPosition = validPositions.some(pos => pos.x === x && pos.y === (gridSize - 1 - y));
         row.push(
-          <div
-            key={`${x},${y}`}
-            style={{
-              width: `${caseSize}px`,
-              height: `${caseSize}px`,
-              border: '1px solid black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              color: isLetterPosition ? 'red' : 'black', // Couleur de la lettre ou du texte
-              backgroundColor: isValidPosition ? 'green' : 'transparent', // Changez la couleur de fond pour les positions valides
-              margin: '2px', // Marge entre les cases
-            }}>
-            {isLetterPosition ? 'X' : ''}
+          <div key={`${x},${y}`}
+            className={`grid-cell ${isValidPosition ? 'valid' : 'invalid'}`}
+          >
+            {isLetterPosition ? <img src={imagePath} alt="Car" style={{ maxWidth: '100%', maxHeight: '100%', transform: `rotate(${carDirection}deg)` }} /> : ''}
           </div>
         );
       }
-      // Ajouter chaque ligne à la grille, en commençant par le bas
       grid.push(<div key={y} style={{ display: 'flex' }}>{row}</div>);
     }
-    // Puisque l'ordre des lignes est inversé, afficher directement la grille construite
     return <div style={{ display: 'flex', flexDirection: 'column' }}>{grid}</div>;
   };
     
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-      {!isConnected && <button onClick={handleConnectClick}>GO</button>}
+    <div className='App-page' >
+      <h2 className='App-header'> VroomVroom </h2>
+      {!isConnected && <Modal onStartClick={handleConnectClick} />}
+      {gameStatus === "victory" && <VictoryModal onRestart={handleRestart} />}
+      {gameStatus === "defeat" && <DefeatModal onRestart={handleRestart} />}
+      {showPenalty && <div className="penalty-message">Pénalité de 1 seconde !</div>}
+      <div className='time'>Temps écoulé: {elapsedTime} secondes</div>
       {renderGrid()}
-      <div>Temps écoulé: {elapsedTime} secondes</div>
+      
     </div>
   );
 }
